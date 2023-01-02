34db3a114c5ea05b856838b1fbe72d7c56a5d3f6
JoeunNAL <euji42@gmail.com>
2023-01-02 18:30:46 +0900

    feat: 바둑판 렌더링 함수 renderGrid 추가

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-/* js */
+function onClick(event) {
+  // 바둑돌 추가하는 함수
+  // console.log(event.target);
+}
+
+function renderGrid(number) {
+  const main = document.querySelector('main');
+
+  for (i = 0; i < number; i++) {
+    const row_container = document.createElement('div');
+    row_container.classList.add('row');
+
+    for (j = 0; j < number; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+
+      const stone = document.createElement('button');
+      stone.classList.add('stone');
+      stone.addEventListener('click', onClick);
+      cell.append(stone);
+
+      row_container.append(cell);
+    }
+
+    main.append(row_container);
+  }
+}
+
+function render() {
+  renderGrid(8);
+}
+
+render();
